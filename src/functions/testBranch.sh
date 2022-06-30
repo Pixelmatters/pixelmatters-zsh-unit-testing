7c67aa402db21d8056a0bc22083ba07224dfd2f7
João Gonçalves <jandretgoncalves@gmail.com>
2022-06-30 17:05:45 +0100

    feat: Improve logging

--- src/functions/testBranch.sh
+++ src/functions/testBranch.sh
@@ -1,7 +1,12 @@
 function testbranch() {
     # Function to Run unit tests on different files between two branches
-
-    CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+    if CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD); then
+    else
+        echo ""
+        echo "🚨 Failed."
+        echo "> This path is not in a git repository."
+        return 
+    fi
 
     if [ $# -eq 1 ] && [ "$1" != "-u" ]; then
         BRANCH_TO_COMPARE=$1
@@ -11,9 +16,10 @@ function testbranch() {
         BRANCH_TO_COMPARE=@targetBranch
     fi
 
-    echo "COMPARING FILES BETWEEN:"
-    echo "$CURRENT_BRANCH (current branch) "
-    echo "$BRANCH_TO_COMPARE (target branch) "
+    echo ""
+    echo "👀 Comparing files between the current and target branch…"
+    echo "> (current branch) $CURRENT_BRANCH"
+    echo "> (target branch) $BRANCH_TO_COMPARE"
 
     CHANGED_FILES=$(git diff --name-status $BRANCH_TO_COMPARE | cut -f2)
 
@@ -25,18 +31,23 @@ function testbranch() {
 
     NR_TRIMMED_FILES=`echo -n $TRIMMED_FILES | wc -m`
     if [ $NR_TRIMMED_FILES -lt 1 ]; then
+        echo ""
         echo "🚨 Failed."
-        echo "There are no changed files between $CURRENT_BRANCH and $BRANCH_TO_COMPARE"
+        echo "> There are no changed files between $CURRENT_BRANCH and $BRANCH_TO_COMPARE"
         return
     fi
 
     CHANGED_FILES_LIST=(`echo ${TRIMMED_FILES}`)
 
-    if [ "$1" != "-u" ]
-    then
+    if [ "$1" != "-u" ]; then
+        echo ""
+        echo "⏲ Running unit tests…"
+        echo ""
         @testRunner ${CHANGED_FILES_LIST}
     else
-        echo "Updating snapshots"
+        echo ""
+        echo "⏲ 📸 Running unit tests and updating snapshots…"
+        echo ""
         @testRunner -u ${CHANGED_FILES_LIST}
     fi
 }
--- src/functions/testChanges.sh
+++ src/functions/testChanges.sh
@@ -1,9 +1,16 @@
 function testchanges() {
     # Function to Run unit tests on all changed files
-    CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+    if CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD); then
+    else
+        echo ""
+        echo "🚨 Failed."
+        echo "> This path is not in a git repository."
+        return 
+    fi
 
-    echo "RUNNING UNIT TESTS FOR CHANGED FILES IN CURRENT BRANCH"
-    echo "$CURRENT_BRANCH (current branch) "
+    echo ""
+    echo "👀 Detecting changed files in current branch…"
+    echo "> (current branch) $CURRENT_BRANCH"
 
     STAGED_FILES=$(git status --porcelain | awk '{ print $2 }')
     TRIMMED_VUE=${STAGED_FILES//.vue/}
@@ -13,18 +20,23 @@ function testchanges() {
 
     NR_TRIMMED_FILES=`echo -n $TRIMMED_FILES | wc -m`
     if [ $NR_TRIMMED_FILES -lt 1 ]; then
+        echo ""
         echo "🚨 Failed."
-        echo "You don't have any changed files yet."
+        echo "> You don't have any changed files yet."
         return
     fi
 
     CHANGED_FILES_LIST=(`echo ${TRIMMED_FILES}`)
 
-    if [ "$1" != "-u" ]
-    then
+    if [ "$1" != "-u" ]; then
+        echo ""
+        echo "⏲ Running unit tests…"
+        echo ""
         @testRunner ${CHANGED_FILES_LIST}
     else
-        echo "Updating snapshots"
+        echo ""
+        echo "⏲ 📸 Running unit tests and updating snapshots…"
+        echo ""
         @testRunner -u ${CHANGED_FILES_LIST}
     fi
 }
